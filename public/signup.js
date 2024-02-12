e5edafdc9d9e4d6eb0c00741c696237062cfc336
colincherney <colinmcherney@gmail.com>
2024-02-12 12:40:05 -0700

    Redirected back to login after succesful signup

--- public/signup.js
+++ public/signup.js
@@ -1,30 +1,31 @@
-document
-  .getElementById("signupForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const formData = new FormData(this);
+document.getElementById("signupForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formData = new FormData(this);
 
-    // Serialize form data into JSON
-    const jsonData = {};
-    for (const [key, value] of formData.entries()) {
-      jsonData[key] = value;
-    }
-    const jsonString = JSON.stringify(jsonData);
+  // Serialize form data into JSON
+  const jsonData = {};
+  for (const [key, value] of formData.entries()) {
+    jsonData[key] = value;
+  }
+  const jsonString = JSON.stringify(jsonData);
 
-    fetch("/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: jsonString,
+  fetch("/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonString,
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      alert(data); // Display the response from the server
+      // Redirect to index.html if signup is successful
+      if (data === "Sign up successful") {
+        window.location.href = "/index.html";
+      }
     })
-      .then((response) => response.text())
-      .then((data) => {
-        alert(data); // Display the response from the server
-        // You can redirect the user to another page after successful signup if needed
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        // Handle errors here
-      });
-  });
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle errors here
+    });
+});
