ebd744ce7d94c6a923e619a5dd368d7bfbaa4ca3
colin11702 <colinmcherney@gmail.com>
2024-02-14 23:11:04 -0700

    Cleaned homepage.js file

--- public/homepage.js
+++ public/homepage.js
@@ -1,21 +1,3 @@
-fetch(`/user`, {
-  method: "GET",
-  credentials: "same-origin",
-})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log("User ID:", data[0].user_id);
-    // Use the user_id as needed in your JavaScript code
-  })
-  .catch((error) => {
-    console.error("There was a problem with your fetch operation:", error);
-  });
-
 // Logout route
 document.getElementById("logout").addEventListener("click", function (event) {
   event.preventDefault(); // Prevent the default behavior of the anchor tag (i.e., navigating to a new page)
