c9d7ccf25ab86e09e1981eb36ea9ce582c4a15fd
colincherney <colinmcherney@gmail.com>
2024-02-21 13:01:42 -0700

    fixed logout button

--- public/logout.js
+++ public/logout.js
@@ -1,14 +1,18 @@
-fetch("/logout", {
-  method: "GET",
-  credentials: "same-origin", // Include cookies in the request
-})
-  .then((response) => {
-    if (response.redirected) {
-      // Redirect to the login page
-      window.location.href = response.url;
-    }
+document.getElementById("logout").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the default link behavior
+  
+  fetch("/logout", {
+    method: "GET",
+    credentials: "same-origin", // Include cookies in the request
   })
-  .catch((error) => {
-    console.error("Error logging out:", error);
-    // Handle error
-  });
+    .then((response) => {
+      if (response.redirected) {
+        // Redirect to the login page
+        window.location.href = response.url;
+      }
+    })
+    .catch((error) => {
+      console.error("Error logging out:", error);
+      // Handle error
+    });
+});
