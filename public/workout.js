1c2ec826065f005acd72ff11f9096771a086a021
colin11702 <colinmcherney@gmail.com>
2024-02-25 11:29:56 -0700

    Fixed so workouts now print onto screen

--- public/workout.js
+++ public/workout.js
@@ -1,27 +1,35 @@
 function exercises() {
-    fetch('/exercises')
-      .then(response => response.json())
-      .then(data => {
-        
-        const exerciseTable = document.getElementById('exerciseTable');
-        const tbody = exerciseTable.querySelector('tbody');
+  fetch("/workouts")
+    .then((response) => response.json())
+    .then((data) => {
+      const exerciseTable = document.getElementById("exerciseTable");
+      const tbody = exerciseTable.querySelector("tbody");
 
-        data.forEach(exercise => {
-          const row = document.createElement('tr');
-          const nameCell = document.createElement('td');
-          const categoryCell = document.createElement('td');
+      data.forEach((exercise) => {
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const categoryCell = document.createElement("td");
+        const weightCell = document.createElement("td");
+        const repsCell = document.createElement("td");
+        const dateCell = document.createElement("td");
 
-          nameCell.textContent = exercise.name;
-          categoryCell.textContent = exercise.category;
+        nameCell.textContent = exercise.exercise_name;
+        categoryCell.textContent = exercise.category;
+        weightCell.textContent = exercise.weight;
+        repsCell.textContent = exercise.reps;
+        dateCell.textContent = exercise.date;
 
-          row.appendChild(nameCell);
-          row.appendChild(categoryCell);
-          tbody.appendChild(row);
-        });
-      })
-      .catch(error => {
-        console.error('Error fetching exercise data:', error);
-      })
-  }
-  
-  exercises();
+        row.appendChild(nameCell);
+        row.appendChild(categoryCell);
+        row.appendChild(weightCell);
+        row.appendChild(repsCell);
+        row.appendChild(dateCell);
+        tbody.appendChild(row);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching exercise data:", error);
+    });
+}
+
+exercises();
